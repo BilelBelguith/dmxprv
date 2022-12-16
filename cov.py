cd2b21d9cb91890b551e5d6ea0dd01bfac780ff5
BilelBelguith <92786342+BilelBelguith@users.noreply.github.com>
2022-12-16 15:13:18 +0100

    Update cov.py

--- cov.py
+++ cov.py
@@ -1,7 +1,7 @@
 import os
 
 os.system("coverage run github_repo.py")
-x = os.system("coverage report --fail-under=80 -m  *.py ")
+x = os.system("coverage report --fail-under=80 -m  github_repo.py ")
 
 if x == 0:
     print("coverage test is succeed ! ")
