7e47a90751d883238ffd901e0a9ee8c12f0b1600
BilelBelguith <92786342+BilelBelguith@users.noreply.github.com>
2022-12-19 16:42:23 +0100

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,50 +1,12 @@
- #import erppeek
-import configparser
-import argparse
+# Define the function to calculate power
+def cal_power(n):
+    return x ** n
 
-parser = argparse.ArgumentParser(description='create new database')
-parser.add_argument('-d', '--dbname', dest='database', nargs='+', help='database name')
-parser.add_argument('-u', '--host', dest='hostname', nargs='+', help='Url odoo')
-parser.add_argument('-m', '--masterpass', dest='masterpass', nargs='+', help='Master password')
-parser.add_argument('-l', '--login', dest='login', nargs='+', help='login')
-parser.add_argument('-p', '--pass', dest='password', nargs='+', help='user password')
+# Take the value of x
+x = 3
+# Define a tuple of numbers
+numbers = [2, 3, 4]
 
-args = parser.parse_args()
-
-
-DATABASE = args.database[0]
-SERVER = args.hostname[0]
-ADMIN_PASSWORD = args.masterpass[0]
-USER_PASS = args.login[0]
-USER_LOGIN = args.password[0]
-
-def installNewDatabase(input):
-    client = erppeek.Client(server=SERVER)
-    if not DATABASE in client.db.list():
-        print("The database does not exist yet, creating one!")
-        client.create_database(ADMIN_PASSWORD, DATABASE, login=USER_LOGIN, user_password=USER_PASS)
-
- 
-        listModules= input.split(",")
-        if (listModules!=[]):
-            installModules(listModules)
-    else:
-        print("The database " + DATABASE + " already exists.")
- 
-        listModules= input.split(",")
-        if (listModules!=[]):
-            installModules(listModules)
-
-def installModules(ListModules):
-    client = erppeek.Client(SERVER, DATABASE, USER_LOGIN, USER_PASS)
-    for i in range(len(ListModules)):
-        print('%s'%ListModules[i])
-        modules = client.modules('%s'%ListModules[i] ,installed=False)
-        print(type(modules))
-        if '%s'%ListModules[i] in modules['uninstalled']:
-            client.install('%s'%ListModules[i])
-
-
-input = input("""what are the modules that you want to install
- """)
-installNewDatabase(input)
+# Calculate the x to the power n using map()
+result = map(cal_power, numbers)
+print(list(result))
