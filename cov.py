82539e24d7929498f76dab430a2dfaec3fad16cb
BilelBelguith <92786342+BilelBelguith@users.noreply.github.com>
2022-12-19 15:09:47 +0100

    Delete cov.py

--- cov.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("coverage run github_repo.py")
-x = os.system("coverage report --fail-under=80 -m  github_repo.py ")
-
-if x == 0:
-    print("coverage test is succeed ! ")
-else:
-    print("Sozdzdzdrry!")
